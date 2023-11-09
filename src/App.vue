e16a492435f085d95b6454458ca2212c1ea042f8
Yurii Rybachok <drummer2009@ukr.net>
2023-11-09 12:03:51 +0200

    setup project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Hello, World!</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+
+body {
+  background-color: #271C36;
+  color: #FFF;
+}
+</style>
